ed223632223eb4e0c2edb3a19fe9427a4a457caf
[Ashutosh Mishra] <[ashutoshmishra7707@gmail.com]>
2023-02-15 15:13:32 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(
+    "mongodb+srv://todo-list:Uku8ekah1rwsNjxi@cluster0.sppqpvp.mongodb.net/devtown?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("connected to MongoDB"))
+  .catch(console.error);
+  const Todo = require('./models/Todo');
+  app.get('/todos', async(req,res)=>{
+    const todo = await Todo.find()
+    res.json(todo);
+  })
+
+  app.post('/todo/new', (req,res)=>{
+    const todo = new Todo({
+        text:req.body.text
+    });
+    todo.save();
+
+    res.json(todo);
+  })
+
+  app.delete('/todo/delete/:id', async(req,res)=>{
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    res.json(result);
+  })
+
+  app.get('/todos/complete/:id', async(req,res)=>{
+    const todo = await Todo.findById(req.params.id);
+    todo.complete = !todo.complete;
+
+    todo.save();
+    res.json(todo);
+  })
+
+  app.put('/todos/complete/:id', async(req,res)=>{
+    const todo = await Todo.findById(req.params.id);
+    todo.complete = !todo.complete;
+
+    todo.save();
+    res.json(todo);
+  })
+
+  app.put('/todo/update/:id', async(req,res) =>{
+    const todo= await Todo.findById(req.params.id);
+    todo.text=req.body.text;
+    todo.save();
+    res.json(todo);
+  })
+app.listen(3004, () => console.log("Server started on port 3004"));
